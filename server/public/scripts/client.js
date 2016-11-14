28a36c5a733244a157c61537f11468c6425907bb
Christopher Mattox <christophermattox@Christophers-MacBook-Pro.local>
2016-11-14 16:04:18 -0600

    killllll meeeeee

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,37 +7,26 @@ console.log('js loaded');
       console.log(data[1].whoseJoke);
       $(".jokesToDom").append("<h2>" + data[0].whoseJoke + "</h2>");
       $(".jokesToDom").append("<p>" + data[0].jokeQuestion + "</p>");
-      $(".jokesToDom").append("<p>" + data[0].punchLine + "</p>")
+      $(".jokesToDom").append("<p>" + data[0].punchLine + "</p>");
       $(".jokesToDom").append("<h2>" + data[1].whoseJoke + "</h2>");
       $(".jokesToDom").append("<p>" + data[1].jokeQuestion + "</p>");
-      $(".jokesToDom").append("<p>" + data[1].punchLine + "</p>")
+      $(".jokesToDom").append("<p>" + data[1].punchLine + "</p>");
       $(".jokesToDom").append("<h2>" + data[2].whoseJoke + "</h2>");
       $(".jokesToDom").append("<p>" + data[2].jokeQuestion + "</p>");
-      $(".jokesToDom").append("<p>" + data[2].punchLine + "</p>")
+      $(".jokesToDom").append("<p>" + data[2].punchLine + "</p>");
     },
     error: function(){
       console.log( 'tis broken good sir' );
     }
 }); // end ajax
 
-$(".submit").on('click', function(){
+$("#submit").on('click', function(){
   event.preventDefault();
+  var jokes  = {};
+  $.each($("jokeForm").serializeArray(), function(i, field){
+    jokes[field.name] = field.vale;
 
-  // var objectToSend = {
-  //   whoseJoke: $('#theAuthor').val(),
-  //   jokeQuestion: $('#theJoke').val(),
-  //   punchLine: $('#thePunchline').val(),
-  //     }; // end objectToSend
-  //
-  //     $.ajax({
-  //       type: 'POST',
-  //       url: '/newjokes',
-  //       data:  objectToSend,
-  //       success: function( data ){
-  //       },
-  //       error: function(){
-  //       }
-  //     }); // end ajax
+  });
 }); //end of submit event
 
 
